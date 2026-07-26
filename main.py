7d84839df6e6718f1ad6abfba97a52a385b60b9d
beelzebul-hub1 <stephendavis664+1@gmail.com>
2026-07-26 00:08:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def export_points_loop():
             # send to website
             try:
                 requests.post(
-                    "https://render-1-ethy.onrender.com/update",
+                    "https://render-b1yk.onrender.com/api/update",
                     json=data,
                     timeout=10
                 )
